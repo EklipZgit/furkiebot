b2f35aaa7b32b335b7ac38d7993e2a13b71ac824
CalistusK <calistussrl@gmail.com>
2014-09-26 16:26:17 -0700

    Centered and skinnied login page a bit better

--- Web/login.php
+++ Web/login.php
@@ -18,7 +18,6 @@
   session_write_close();
   ?>
 	<div class="skinny">
-		<div class="container">
 			<center><h3 class="heading">Log in with your FurkieBot registration details!</h3></center>
 				<form class="form-signin" id="form1" name="form1" role="form" method="post" action="loginsubmit.php">
 				<input type="username" class="form-control" placeholder="Username" name="username" id="username" required autofocus>
@@ -26,15 +25,14 @@
 				<button class="btn btn-lg btn-primary btn-block" type="submit" value="Submit" >Log in</button>
 				</form>
 			<p><b>Haven't registered with FurkieBot yet? <a href="http://eklipz.us.to/cmr/register.html">Learn how to register</a></b></p>
-			<p>Forgot your CMR password? Just follow the registration instructions above to register a new password. Don't worry, this won't reset your race history or anything like that.</p>
+			<p>Forgot your CMR password? Just follow the registration instructions above to register a new password. <br>Don't worry, this won't reset your race history or anything like that.</br></p>
 			<p>Forgot your SpeedRunsLive identify password? You will need to follow their instructions for resetting below:</p>
 				
 				<div class="well">
-				<p>If you forgot your password, you can request an e-mail with instructions to reset it using "/nickserv resetpass (nickname)". The e-mail may be filtered by your spam filter.</p>
+				<p>If you forgot your password, you can request an e-mail with instructions to reset it using <br>"/nickserv resetpass (nickname)". The e-mail may be filtered by your spam filter.</br></p>
 
 				<p>Switched SRL nicks and need your info transferred onto a new nick? Talk to an admin. We won't be happy with you, but for now it is doable.</p>
-			</div>
-		</div>
+				</div>
     </div>
   </body>
 </html>
