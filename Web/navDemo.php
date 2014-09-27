dddbae18890a4550d3f2f5443184bba5ae725a58
EklipZgit <tdraketdraketdrake@yahoo.com>
2014-09-26 21:29:21 -0700

    New navbar stuff. Use navDemo.php to test.
    navbar.php moved to WebInclude. Use navDemo.php to test the navbar.

---
+++ Web/navDemo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>NAVBAR DEMO</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="css/starter-template.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<?php include "../WebInclude/navbar.php"; ?>
+
+</body>
+<footer>
+</footer>
---
+++ WebInclude/navbar.php
@@ -0,0 +1,41 @@
+
+<?php
+	$ek = "http://eklipz.us.to/cmr/";
+
+	//THIS IS WHERE YOU ADD NEW PAGES. They will auto add to the sidebar.
+	$navlist = [
+	"Home" => "${ek}index.html",
+	"FAQ" => "${ek}about.html",
+	"Upload map" => "${ek}map.php",
+	"Testers" => "${ek}maptest.php",
+	//    "Home" => "${ek}index.html",      //add more as more shit gets added to site
+	];
+?>
+
+
+
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand">Custom Map Races</a>
+	</div>
+
+	<div class="collapse navbar-collapse">
+		<ul class="nav navbar-nav">
+
+			<?php 
+
+				foreach ($navlist as $key => $value) {
+					echo '<li class="active"><a href="' . $value . '">' . $key . '</a></li>';
+				}
+
+			?>
+			
+		</ul>
+	</div><!--/.nav-collapse -->
+</div>
