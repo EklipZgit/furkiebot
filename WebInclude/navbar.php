24c66027bae44a3e3b031a84dcf824b3422f42d6
EklipZgit <tdraketdraketdrake@yahoo.com>
2014-09-26 21:39:09 -0700

    fixed some navbar stuff

--- WebInclude/navbar.php
+++ WebInclude/navbar.php
@@ -10,32 +10,42 @@
 	"Testers" => "${ek}maptest.php",
 	//    "Home" => "${ek}index.html",      //add more as more shit gets added to site
 	];
+
+
+
+	function displayNavbar(string $currentpage) {
+
 ?>
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand">Custom Map Races</a>
+			</div>
 
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
 
+					<?php 
 
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-		<a class="navbar-brand">Custom Map Races</a>
-	</div>
+						foreach ($navlist as $key => $value) {
+							if ($key == $currentpage) {
+								echo '<li class="active">' . $key . '</li>';
+							} else {
+								echo '<li class="inactive"><a href="' . $value . '">' . $key . '</a></li>';
+							}
+						}
 
-	<div class="collapse navbar-collapse">
-		<ul class="nav navbar-nav">
-
-			<?php 
-
-				foreach ($navlist as $key => $value) {
-					echo '<li class="active"><a href="' . $value . '">' . $key . '</a></li>';
-				}
-
-			?>
-			
-		</ul>
-	</div><!--/.nav-collapse -->
+					?>
+					
+				</ul>
+			</div><!--/.nav-collapse -->
 </div>
+
+<?php
+	}
+?>
