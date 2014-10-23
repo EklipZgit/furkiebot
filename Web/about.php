489df5ce46fa05c4190d4657982fd2d7cbf2f67c
EklipZgit <tdraketdraketdrake@yahoo.com>
2014-10-23 13:33:51 -0700

    fixed about some more

--- Web/about.php
+++ Web/about.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <?php include "../WebInclude/navbar.php"; // YOU NEED TO INCLUDE THIS FILE AT THE TOP OF THE PAGE.
 ?> 
@@ -69,17 +68,17 @@
 			<h3 class="ml">Maps</h3>
 			<p class="ml">
 				<li class="ml">
-				Anybody can make a CMR map! By making maps for the CMR, you get the advantage of knowing the maps you made ahead of time for the race! <b><a href="tester.php">You must first read the Map Making rules carefully</a></b>, however. Your map will not be accepted if you do not follow the rules!
+					Anybody can make a CMR map! By making maps for the CMR, you get the advantage of knowing the maps you made ahead of time for the race! <b><a href="tester.php">You must first read the Map Making rules carefully</a></b>, however. Your map will not be accepted if you do not follow the rules!
 				</li>
 
 				<li class="ml">
 					You can either be there to upload your map to atlas when the race starts, or FurkieBot will publish your map for you (and give you credit) if you are not in the IRC channel within 10 minutes of the designated race time (20 minutes after the time listed).
 				</li>
 				<li class="ml">
-				You just need to register with FurkieBot <a href="http://eklipz.us.to/cmr/register.php">(More info here)</a> and then use the Map submission form on this site to submit your map!
+					You just need to register with FurkieBot <a href="http://eklipz.us.to/cmr/register.php">(More info here)</a> and then use the Map submission form on this site to submit your map!
 				</li>
 				<li class="ml">
-				Maps do not need to be decorated before submission, but the gameplay should be done. When decorating after submission there can be severe penalties if you:
+					Maps do not need to be decorated before submission, but the gameplay should be done. When decorating after submission there can be severe penalties if you:
 				<li class="ml">Make parts of the map too dark to see</li>
 				<li class="ml">Zoom the camera out further than it was in the submission</li>
 				<li class="ml">Change the color scheme so that dust, enemies, or the player are harder to see</li>
