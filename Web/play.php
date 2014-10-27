3597df8a792dd4dc794c2402bf4b57601cd7402f
EklipZgit <tdraketdraketdrake@yahoo.com>
2014-10-27 14:48:46 -0700

    shorthand versions of install and play

---
+++ Web/play.php
@@ -0,0 +1,13 @@
+<?php 
+include_once "../WebInclude/funcs.php";
+//done until additional map metadata is added.
+ensureSession();
+$mapID = trim(urldecode($_GET['id']));
+$mapName = trim(urldecode($_GET['name']));
+header("Location: dustforce://installPlay/" . $mapID . "/" . $mapName);
+// header("X-Sendfile: $mappath");
+// header("Content-type: application/octet-stream");
+// header('Content-Disposition: attachment; filename="' . basename($mappath) . '"');
+exit;
+
+?>
